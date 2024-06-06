2cfa356218961e040eb7f3a764a26bb508b66a5c
Berkay Dişli <berkay.dsli@gmail.com>
2024-06-06 16:36:29 +0300

    Update SwiftLogger.swift

--- Sources/SwiftLogger/SwiftLogger.swift
+++ Sources/SwiftLogger/SwiftLogger.swift
@@ -1,6 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+import SwiftUI
+
 public enum LogLevel: String {
     case info = "INFO"
     case warning = "WARNING"
